1ec7a51ea615371089b8fafa41c9c0a7a0ebd60e
Karlo Kukuruzovic <X00154934@mytudublin.ie>
2021-11-15 14:30:25 +0000

    CA1 error fixed

--- articles/urls.py
+++ articles/urls.py
@@ -8,7 +8,7 @@ from .views import (
 )
 
 urlpatterns = [
-    path('<int:pk>/edit', ArticleUpdateView.as_view(), name='article_edite'),
+    path('<int:pk>/edit', ArticleUpdateView.as_view(), name='article_edit'),
     path('<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
     path('<int:pk>/delete/', ArticleDeleteView.as_view(), name='article_delete'),
     path('new/', ArticleCreateView.as_view(), name='article_new'),
