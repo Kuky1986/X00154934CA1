e826614ef3f72ac1ad82b6e30562982edb290692
Karlo Kukuruzovic <X00154934@mytudublin.ie>
2021-11-15 10:06:12 +0000

    CA1 3rd commit

--- CarBlog/settings.py
+++ CarBlog/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts'
+    #local
+    'accounts',
+    'pages',
+    #3rd party
+    'crispy_forms',
+    'crispy_bootstrap5'
+
 ]
 
 MIDDLEWARE = [
@@ -127,3 +133,5 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
